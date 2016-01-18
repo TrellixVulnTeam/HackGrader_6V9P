9ba85b13afa4fdd40cf6d5bd42c7771c4abb6893
RadoRado <radorado@hackbulgaria.com>
2016-01-19 01:01:02 +0200

    Add structure for tests

---
+++ source/grader/tests/test_graders.py
@@ -0,0 +1,4 @@
+import unittest
+
+class TestGraders(unittest.TestCase):
+    pass
---
+++ source/grader/tests/test_start.py
@@ -0,0 +1,21 @@
+import unittest
+
+
+class TestGraderStart(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_start_with_no_data_json(self):
+        pass
+
+    def test_start_with_no_language_key_in_data_json(self):
+        pass
+
+    def test_start_with_not_present_solution_file(self):
+        pass
+
+    def test_start_with_not_present_tests_file(self):
+        pass
