5f890386db6f760602afc401af1a93796241e416
Pavlin Gergov <gergov.pavlin@gmail.com>
2016-01-18 16:31:45 +0200

    Refactor get nonce view
    * Fix post request to get
    * Send data in headers for consistency

--- source/api_auth/views.py
+++ source/api_auth/views.py
@@ -5,18 +5,17 @@ from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import csrf_exempt
 
 
-@require_http_methods(["POST"])
 @csrf_exempt
+@require_http_methods(["GET"])
 def get_req_and_resource_nonce(request):
-    if request.POST:
-        request_info = request.POST["request_info"]
-        user_key = request.POST["user_key"]
+    request_info = request.META.get('HTTP_REQUEST_INFO')
+    user_key = request.META.get('HTTP_X_USER_KEY')
 
-        api_request = get_object_or_404(ApiRequest, user__key=user_key, request_info=request_info)
-        nonce = api_request.nonce
+    api_request = get_object_or_404(ApiRequest, user__key=user_key, request_info=request_info)
+    nonce = api_request.nonce
 
-        data = {
-         'nonce': nonce
-        }
+    data = {
+     'nonce': nonce
+    }
 
-        return JsonResponse(data, safe=False)
+    return JsonResponse(data, safe=False)
