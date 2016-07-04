6b6bf67a0908f83fd84e1ee3dbbe8e9d36e21397
Alexander Nadjarian <alexandernadjarian@gmail.com>
2016-07-04 11:21:27 +0300

    fix get_req_and_resource_nonce in api_auth views (#9)

--- source/api_auth/views.py
+++ source/api_auth/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import csrf_exempt
+from django.db.models import Max
 
 
 @csrf_exempt
@@ -11,11 +12,12 @@ def get_req_and_resource_nonce(request):
     request_info = request.META.get('HTTP_REQUEST_INFO')
     user_key = request.META.get('HTTP_X_USER_KEY')
 
-    api_request = get_object_or_404(ApiRequest, user__key=user_key, request_info=request_info)
-    nonce = api_request.nonce
+    api_request = ApiRequest.objects.filter(user__key=user_key, request_info=request_info).aggregate(Max('nonce'))
+    last_nonce = api_request['nonce__max']
+    valid_nonce = last_nonce + 1
 
     data = {
-     'nonce': nonce
+     'nonce': valid_nonce
     }
 
     return JsonResponse(data, safe=False)
