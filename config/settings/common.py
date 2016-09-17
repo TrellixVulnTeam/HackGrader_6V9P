7673124730e9fcf15215b51f924bfc3aec8ef45c
Ivaylo Bachvarof <bachvarof@gmail.com>
2016-09-17 16:37:49 +0300

    Remove static dir. No static files in project

--- config/settings/common.py
+++ config/settings/common.py
@@ -160,11 +160,6 @@ STATIC_ROOT = str(ROOT_DIR('staticfiles'))
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#static-url
 STATIC_URL = '/static/'
 
-# See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
-STATICFILES_DIRS = (
-    str(APPS_DIR.path('static')),
-)
-
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
