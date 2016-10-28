4fb4c3e0f4ca54135945eded4beb85dc1ef3d842
alexnad <alexnad@gmail.com>
2016-10-28 12:11:11 +0300

    add command to provision database with initial data

---
+++ hacktester/seed/management/commands/provision_initial_data.py
@@ -0,0 +1,42 @@
+from django.core.management.base import BaseCommand
+
+from hacktester.tester.models import ArchiveType, TestType, Language
+
+
+OUTPUT_CHECKING = "output_checking"
+UNITTEST = "unittest"
+
+TEST_TYPES = [OUTPUT_CHECKING, UNITTEST]
+
+JAVA = "java"
+JAVA_EXTENSION = ".java"
+PYTHON = "python"
+PYTHON_EXTENSION = ".py"
+RUBY = "ruby"
+RUBY_EXTENSION = ".rb"
+
+
+LANGUAGES = (
+    (JAVA, JAVA_EXTENSION),
+    (PYTHON, PYTHON_EXTENSION),
+    (RUBY, RUBY_EXTENSION)
+)
+
+ARCHIVE_TYPES = ("tar_gz")
+
+
+class Command(BaseCommand):
+    def add_archive_types(self):
+        for archive_type in ARCHIVE_TYPES:
+            ArchiveType.objects.create(value=archive_type)
+
+    def add_languages(self):
+        for language, extension in LANGUAGES:
+            Language.objects.create(name=language, extension=extension)
+
+    def add_test_types(self):
+        for test_type_value in TEST_TYPES:
+            TestType.objects.create(value=test_type_value)
+
+    def handle(self):
+        self.add_test_types()
