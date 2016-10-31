246129b4ed8838ae83e33885b63402ff86a56adf
alexnad <alexnad@gmail.com>
2016-10-31 16:50:11 +0200

    change the check_result response json format

--- hacktester/runner/return_codes.py
+++ hacktester/runner/return_codes.py
@@ -8,3 +8,17 @@ RUN_EXCEPTION = 6
 UNKNOWN_EXCEPTION = 7
 CALLED_PROCESS_ERROR = 8
 UNSUPPORTED_TEST_TYPE = 9
+
+
+RETURN_CODE_OUTPUT = {
+    OK: "OK",
+    COMPILATION_ERROR: "compilation error",
+    TIME_LIMIT_ERROR: "time limit reached",
+    MEMORY_LIMIT_ERROR: "memory limit reached",
+    WRONG_ANSWER: "incorrect answer",
+    LINT_ERROR: "lint error",
+    RUN_EXCEPTION: "runtime error",
+    UNKNOWN_EXCEPTION: "test failed to run",
+    CALLED_PROCESS_ERROR: "test failed to run",
+    UNSUPPORTED_TEST_TYPE: "test type unsupported"
+}
--- hacktester/tester/utils.py
+++ hacktester/tester/utils.py
@@ -1,26 +1,35 @@
 from urllib.parse import urlsplit
 from hacktester.runner.settings import UNITTEST, OUTPUT_CHECKING
 from .models import RunResult
+from hacktester.runner.return_codes import RETURN_CODE_OUTPUT
 
 
 def get_run_results(run, run_results):
     if run.test_type.value == UNITTEST:
         result = run_results[0]
-        data = {'run_status': run.status,
-                'result_status': result.status,
-                'run_id': run.id,
-                'output': result.output,
-                'returncode': result.returncode}
+        test_output = RETURN_CODE_OUTPUT.get(result.returncode, "unknown test run error")
+        data = {
+            'run_status': run.status,
+            'result_status': result.status,
+            'run_id': run.id,
+            'returncode': result.returncode,
+            'output': {"test_status": test_output,
+                       "test_output": result.output}
+            }
 
     elif run.test_type.value == OUTPUT_CHECKING:
         output = []
         status = RunResult.PASSING
 
         for result in run_results:
-            if result.output != 'OK':
+            if result.status != RunResult.PASSING:
                 status = RunResult.FAILED
 
-            output.append(result.output)
+            test_output = RETURN_CODE_OUTPUT.get(result.returncode, "unknown test run error")
+            test_output = {"test_status": test_output,
+                           "test_output": result.output}
+
+            output.append(test_output)
 
         if len(output) == 1:
             output = output[0]
