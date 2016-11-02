b02e3fe10ed3e5b75e6468705312640cde062f8d
alexnad <alexnad@gmail.com>
2016-11-02 18:46:20 +0200

    output field in json response for output checking is now always a list

--- hacktester/tester/utils.py
+++ hacktester/tester/utils.py
@@ -7,12 +7,12 @@ from hacktester.runner.return_codes import RETURN_CODE_OUTPUT
 def get_run_results(run, run_results):
     if run.test_type.value == UNITTEST:
         result = run_results[0]
-        test_output = RETURN_CODE_OUTPUT.get(result.returncode, "unknown test run error")
+        test_status = RETURN_CODE_OUTPUT.get(result.returncode, "unknown test run error")
         data = {
             'run_status': run.status,
             'result_status': result.status,
             'run_id': run.id,
-            'output': {"test_status": test_output,
+            'output': {"test_status": test_status,
                        "test_output": result.output}
             }
 
@@ -24,14 +24,11 @@ def get_run_results(run, run_results):
             if result.status != RunResult.PASSING:
                 status = RunResult.FAILED
 
-            test_output = RETURN_CODE_OUTPUT.get(result.returncode, "unknown test run error")
-            test_output = {"test_status": test_output,
+            test_status = RETURN_CODE_OUTPUT.get(result.returncode, "unknown test run error")
+            test_output = {"test_status": test_status,
                            "test_output": result.output}
 
             output.append(test_output)
-
-        if len(output) == 1:
-            output = output[0]
 
         data = {'run_status': run.status,
                 'result_status': status,
