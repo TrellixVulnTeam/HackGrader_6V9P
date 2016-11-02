9142630f6bb0aba33b4b8ee77775e767a7ee0c5d
Ivaylo Bachvarof <bachvarof@gmail.com>
2016-11-02 13:06:41 +0200

    Fix the deploy.rb

--- deploy/config/deploy.rb
+++ deploy/config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, 'git@github.com:HackBulgaria/HackTester.git'
 ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/hack/grader'
+set :deploy_to, '/hack/hacktester'
 
 # Default value for :scm is :git
 # set :scm, :git
@@ -24,7 +24,7 @@ set :deploy_to, '/hack/grader'
 
 # Default value for :linked_files is []
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
-set :linked_files, fetch(:linked_files, []).push('source/HackTester/local_settings.py')
+# set :linked_files, fetch(:linked_files, []).push('source/HackTester/local_settings.py')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('static', 'media')
@@ -38,44 +38,38 @@ set :linked_dirs, fetch(:linked_dirs, []).push('static', 'media')
 namespace :deploy do
   task :pip_install do
     on roles(:all) do |h|
-      execute "#{fetch :deploy_to}/shared/virtualenv/bin/pip install -r #{fetch :deploy_to}/current/requirements/common.txt"
+      execute "#{fetch :deploy_to}/shared/virtualenv/bin/pip install -r #{fetch :deploy_to}/current/requirements/base.txt"
       execute "#{fetch :deploy_to}/shared/virtualenv/bin/pip install -r #{fetch :deploy_to}/current/requirements/production.txt"
     end
   end
 
   task :run_migrations do
     on roles(:all) do |h|
-      execute "#{fetch :deploy_to}/shared/virtualenv/bin/python3 #{fetch :deploy_to}/current/source/manage.py migrate --noinput"
+      execute "#{fetch :deploy_to}/shared/virtualenv/bin/python3 #{fetch :deploy_to}/current/manage.py migrate --noinput"
     end
   end
 
   task :run_collect_static do
     on roles(:all) do |h|
-      execute "#{fetch :deploy_to}/shared/virtualenv/bin/python3 #{fetch :deploy_to}/current/source/manage.py collectstatic --noinput"
+      execute "#{fetch :deploy_to}/shared/virtualenv/bin/python3 #{fetch :deploy_to}/current/manage.py collectstatic --noinput"
     end
   end
 
   task :rebuild_docker do
     on roles(:all) do |h|
-      execute "docker build -t grader #{fetch :deploy_to}/current/source/docker/."
+      execute "docker build -t grader #{fetch :deploy_to}/current/hacktester/docker/."
     end
   end
 
-  task :stop_grader do
+  task :restart_grader do
     on roles(:all) do |h|
-      execute "sudo stop grader"
-    end
-  end
-
-  task :start_grader do
-    on roles(:all) do |_h|
-      execute "sudo start grader"
+      execute "sudo restart grader || sudo start grader"
     end
   end
 
   task :restart_celery do
     on roles(:all) do |h|
-      execute "sudo restart celery"
+      execute "sudo restart celery || sudo start celery"
     end
   end
 
@@ -83,7 +77,6 @@ namespace :deploy do
   after :pip_install, :run_migrations
   after :run_migrations, :rebuild_docker
   after :run_migrations, :run_collect_static
-  after :run_collect_static, :stop_grader
-  after :stop_grader, :restart_celery
-  after :restart_celery, :start_grader
+  after :run_collect_static, :restart_grader
+  after :restart_grader, :restart_celery
 end
