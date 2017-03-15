05fb9b64144ca15e97bea79925485cb8b7a2fb51
martin95056 <martinangelov056@gmail.com>
2017-03-15 10:55:00 +0200

    Fix extract_test_tar_gz_form_bytes(). Check for existing requirements files.

--- hacktester/tester/tasks/common_utils.py
+++ hacktester/tester/tasks/common_utils.py
@@ -4,9 +4,10 @@ import base64
 import shutil
 import os
 
+from django.conf import settings
+
 from ..models import TestRun
 from hacktester.tester.models import RunResult
-from hacktester.runner.exceptions import RequirementsFailedInstalling
 
 
 def get_result_status(returncode):
@@ -38,25 +39,38 @@ class ArchiveFileHandler:
     def extract_test_tar_gz_form_bytes(cls, byte_string, path_to_extract, test_file_name):
         with tempfile.TemporaryDirectory() as temp_dir:
             cls.extract_tar_gz_from_bytes(byte_string, path_to_extract=temp_dir)
-
-            # Copy test file from temp_dir in test file in path_to_extract
-            # If path_to_extract directory already contains test_file_name is is overriden.
+            """
+            Copy test file from temp_dir in test file in path_to_extract
+            If path_to_extract directory already contains test_file_name is is overriden.
+            """
             shutil.copyfile(os.path.join(temp_dir, test_file_name),
                             os.path.join(path_to_extract, test_file_name))
 
-            # Get data from test requirements.txt file (the one from temp_dir)
-            if os.path.exists(os.path.join(temp_dir, "requirements.txt")):
-                with open(os.path.join(temp_dir, "requirements.txt"), "r") as test_req_file:
-                    data = test_req_file.read()
+            test_requirements_file = os.path.join(temp_dir, settings.DJANGO_DEPENDENCIES_FILENAME)
+            solution_requirements_file = os.path.join(path_to_extract, settings.DJANGO_DEPENDENCIES_FILENAME)
+            """
+            Get data from test requirements.txt file (the one from temp_dir)
+            if that file exists.
+            """
+            if os.path.exists(test_requirements_file):
+                with open(test_requirements_file, "r") as f:
+                    test_req_data = f.read()
+            """
+            Get data from solution requirements.txt file (the one from path_to_extract)
+            if that file exists.
+            """
+            if os.pat.exists(solution_requirements_file):
+                with open(solution_requirements_file, "r") as f:
+                    solution_req_data = f.read()
 
-                data = "\n" + data
-
-            else:
-                raise RequirementsFailedInstalling("Requirements for test.py not provided")
-
-            # Append data from test requirements.txt to requirements.txt in path_to_extract
-            with open(os.path.join(path_to_extract, "requirements.txt"), "a") as solution_req_file:
-                solution_req_file.write(data)
+            requirements_data = solution_req_data + '\n' + test_req_data
+            """
+            At the end we want to have single requirements.txt file. We open solution
+            requirements file in "w+" mode which means that it will be overriden if exists
+            and created if does not exist.
+            """
+            with open(solution_requirements_file, "w+") as f:
+                f.write(requirements_data)
 
     @classmethod
     def extract(cls, archive_type, path_to_archive, path_to_extract="."):
