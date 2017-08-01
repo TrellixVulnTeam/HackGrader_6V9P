00d328cb2c86c13dece3f6a55ac02819659b9706
Nimor111 <georgi.bojinov@hotmail.com>
2017-08-01 11:24:57 +0300

    Refactor validators

--- hacktester/tester/serializers.py
+++ hacktester/tester/serializers.py
@@ -1,29 +1,15 @@
 from rest_framework import serializers
 
 from .models import TestRun, Language, TestType
+from .validators import validate_language, validate_test_type
 
 
 class TestRunSerializer(serializers.ModelSerializer):
     extra_options = serializers.DictField(required=False)
-    language = serializers.CharField()
-    test_type = serializers.CharField()
+    language = serializers.CharField(validators=[validate_language])
+    test_type = serializers.CharField(validators=[validate_test_type])
     test = serializers.CharField(required=False)
     solution = serializers.CharField(required=False)
-
-    def validate(self, attrs):
-        language_name = self.initial_data.get('language')
-        test_type = self.initial_data.get('test_type')
-        language = Language.objects.filter(name=language_name).first()
-        if language is None:
-            raise serializers.ValidationError(
-                "Language {} not supported. Please check GET /supported_languages".format(language_name)
-            )
-        test = TestType.objects.filter(value=test_type).first()
-        if test is None:
-            raise serializers.ValidationError(
-                "Test type {} not supported. Please check GET /supported_test_types".format(test_type)
-            )
-        return attrs
 
     class Meta:
         model = TestRun
---
+++ hacktester/tester/validators.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+
+from .models import Language, TestType
+
+
+def validate_language(language):
+    language_name = language
+    language = Language.objects.filter(name=language_name).first()
+    if language is None:
+        raise serializers.ValidationError(
+            "Language {} not supported. Please check GET /supported_languages".format(language_name)
+        )
+
+
+def validate_test_type(test_type):
+    test = TestType.objects.filter(value=test_type).first()
+    if test is None:
+        raise serializers.ValidationError(
+            "Test type {} not supported. Please check GET /supported_test_types".format(test_type)
+        )
